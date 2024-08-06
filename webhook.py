02568ac6fe6432b7b9ef4750d3410fde00e0f897
azlan <azlan.nor@gmail.com>
2024-08-06 02:33:11 +0000

    remove store echo

--- webhook.py
+++ webhook.py
@@ -47,8 +47,6 @@ def register(webhook_id):
 
 @app.post('/echo')
 def echo():
-    db = sqlite_utils.Database('sql.db')
-    db['request'].insert({ 'id': webhook_id, 'payload': json.dumps(request.json) }, pk='id', replace=True)
     return request.json
 
 @app.get('/history/<webhook_id>/<history_type>')
